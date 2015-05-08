a3f330d43177570150fe0ad4f3dc7a555eafa6a8
Adam McFadden <mcfadden.113@gmail.com>
2015-05-08 08:24:51 -0700

    adds spec_helper, and secs for stylist and client classes.

---
+++ spec/stylist_spec.rb
@@ -0,0 +1,29 @@
+describe (Stylist) do
+  describe("#name") do
+
+  end
+
+#   describe("#id") do
+#
+#   end
+#
+#   describe(".all") do
+#
+#   end
+#
+#   describe("#save") do
+#
+#   end
+#
+#   describe(".find") do
+#
+#   end
+#
+#   describe("#delete") do
+#
+#   end
+#
+#   describe("#update") do
+#
+#   end
+end
