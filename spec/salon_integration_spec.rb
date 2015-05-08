6ee0e440eba37ed0fd7d4e8215e2daaf7ee7f3fa
Adam McFadden <mcfadden.113@gmail.com>
2015-05-08 14:21:43 -0700

    add a second integration spec testing adding a client, passes.

--- spec/salon_integration_spec.rb
+++ spec/salon_integration_spec.rb
@@ -2,12 +2,19 @@ require 'spec_helper'
 Capybara.app = Sinatra::Application
 
 
-describe('The add new stylist path', {:type => :feature}) do
+describe('The add new path', {:type => :feature}) do
   it('will add a new stylist and display that stylist in the correct location') do
     visit('/')
     click_button('add_stylist')
     fill_in('stylist_entry', :with => 'Bender')
     click_button('stylist_submit')
     expect(page).to have_content('Bender')
+  end
+  it('will add a new stylist and display that stylist in the correct location') do
+    visit('/')
+    click_button('add_client')
+    fill_in('client_entry', :with => 'Leela')
+    click_button('client_submit')
+    expect(page).to have_content('Leela')
   end
 end
