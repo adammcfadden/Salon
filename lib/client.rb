dd12be41bf38e00faad86123f9ce7ee569c49272
Adam McFadden <mcfadden.113@gmail.com>
2015-05-08 11:53:29 -0700

    add all methods present in the stylist class to the client class, add corresponding specs. all specs passing.

--- lib/client.rb
+++ lib/client.rb
@@ -5,4 +5,41 @@ class Client
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
   end
+
+  define_singleton_method(:all) do
+    client_table = DB.exec("SELECT * FROM clients;")
+    client_array = []
+    client_table.each() do |object|
+      name = object.fetch('name')
+      id = object.fetch('id').to_i()
+      client = Client.new({:name => name, :id => id})
+      client_array.push(client)
+    end
+    client_array
+  end
+
+  define_method(:save) do
+    returns = DB.exec("INSERT INTO clients (name) VALUES ('#{self.name()}') RETURNING id;")
+    @id = returns.first().fetch('id').to_i
+  end
+
+  define_method(:==) do |other|
+    self.name() == other.name()
+  end
+
+  define_singleton_method(:find) do |identification|
+    client = DB.exec("SELECT * FROM clients WHERE id = #{identification};")
+    name = client.first().fetch('name')
+    Client.new({:name => name, :id => identification})
+  end
+
+  define_method(:delete) do
+    DB.exec("DELETE FROM clients WHERE id = #{self.id()};")
+  end
+
+  define_method(:update) do |attributes|
+    @name = attributes.fetch(:name, @name)
+    @id = self.id()
+    DB.exec("UPDATE clients SET name = '#{@name}' WHERE id = #{@id}")
+  end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -3,31 +3,66 @@ require 'spec_helper'
 describe(Client) do
   describe('#name') do
     it('will return the name of a client') do
-      test_client = Client.new({:name => 'hermes', :id => nil})
-      expect(test_client.name()).to(eq('hermes'))
+      test_client = Client.new({:name => 'archer', :id => nil})
+      expect(test_client.name()).to(eq('archer'))
     end
   end
-  # describe('#id') do
-  #
-  # end
-  #
-  # describe('.all') do
-  #
-  # end
-  #
-  # describe('#save') do
-  #
-  # end
-  #
-  # describe('.find') do
-  #
-  # end
-  #
-  # describe('#delete') do
-  #
-  # end
-  #
-  # describe('#update') do
-  #
-  # end
+
+  describe(".all") do
+    it('will return an array of all clients objects in the database') do
+      expect(Client.all()).to(eq([]))
+    end
+  end
+
+  describe("#id") do
+    it('will return the id of a client') do
+      test_client = Client.new({:name => 'pam', :id => nil})
+      test_client.save()
+      expect(test_client.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe("#save") do
+    it('will save a client to the database') do
+      test_client = Client.new({:name => 'carol', :id => nil})
+      test_client.save()
+      expect(Client.all()).to(eq([test_client]))
+    end
+  end
+
+  describe('#==') do
+    it('makes the == operator true if names are the same') do
+      test_client1 = Client.new({:name => 'malory', :id => nil})
+      test_client2 = Client.new({:name => 'malory', :id => nil})
+      expect(test_client1).to(eq(test_client2))
+    end
+  end
+
+  describe(".find") do
+    it('will return a client object associated with the given id') do
+      test_client1 = Client.new({:name => 'ray', :id => nil})
+      test_client1.save()
+      test_client2 = Client.new({:name => 'cyril', :id => nil})
+      test_client2.save()
+      expect(Client.find(test_client2.id())).to(eq(test_client2))
+    end
+  end
+
+  describe("#delete") do
+    it('will delete a client object from the database') do
+      test_client1 = Client.new({:name => 'kreiger', :id => nil})
+      test_client1.save()
+      test_client1.delete()
+      expect(Client.all()).to(eq([]))
+    end
+  end
+
+  describe("#update") do
+    it('will allow you to update a client object in the database') do
+      test_client1 = Client.new(:name => 'brent', :id => nil)
+      test_client1.save()
+      test_client1.update({:name => 'barry'})
+      expect(test_client1.name()).to(eq('barry'))
+    end
+  end
 end
